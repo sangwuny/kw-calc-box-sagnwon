e72258da254ddf9f0f9fa1e8708572ee1a5b50ef
sangwuny <sangwuny@naver.com>
2023-11-01 00:29:23 +0900

    link

--- lib.js
+++ lib.js
@@ -3,23 +3,25 @@ function avg(numbers) {
     return s / numbers.length;
 }
 
-function prime(numbers){
+function prime(numbers) {
 
-    let num = numbers-1;
-
-    while(num!== 1){
-        if(numbers % num==0) return false;
-        num--;
+    for(let i=2; i<numbers;i++){
+        if(numbers%i==0){
+            return false;
+        }
     }
 
     return true;
 }
 
 
-function fact(numbers){
-    if(numbers === 1) return 1;
-
-    return numbers*fact(numbers-1);
+function fact(numbers) {
+    if (numbers <= 1) {
+        return 1;
+    }
+    else {
+        return numbers * fact(numbers - 1);
+    }
 }
 
 module.exports = {
