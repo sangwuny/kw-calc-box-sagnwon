c34ff52d5b445a0920640e91c9f7246d2e0178f3
sangwuny <sangwuny@naver.com>
2023-10-31 23:37:08 +0900

    add prime and fact

--- lib.js
+++ lib.js
@@ -2,12 +2,26 @@ function avg(numbers) {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
 }
-function prime(num) {
-    return false;
+
+function prime(numbers){
+
+    let num = numbers-1;
+
+    while(num!== 1){
+        if(numbers % num==0) return false;
+        num--;
+    }
+
+    return true;
 }
-function fact(num) {
-    return -1;
+
+
+function fact(numbers){
+    if(numbers === 1) return 1;
+
+    return numbers*fact(numbers-1);
 }
+
 module.exports = {
     avg,
     prime,
--- lib.test.js
+++ lib.test.js
@@ -7,3 +7,11 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("17 should be prime",()=>{
+    expect(lib.prime(17)).toBe(true);
+});
+
+test("factorial 16 must be 20,922,789,888,000",()=>{
+    expect(lib.fact(16)).toBe(20922789888000);
+});
